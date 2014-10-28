d3ec2909b52e69e2b9ca39d0e12bf50fa42f4920
dangeryoder <dangeryoder@gmail.com>
2014-10-28 15:18:33 -0500

    100 pt
    i know that the ball shifts down 10 pixels everytime it wraps, i did
    that intentionally

--- lab11-100pt.py
+++ lab11-100pt.py
@@ -10,27 +10,19 @@
 
 from Tkinter import *
 root = Tk()
-# Create the canvas widget
 drawpad = Canvas(root, width=800,height=600, background='white')
 drawpad.grid(row=0, column=0)
+circle = drawpad.create_oval(10, 10, 50, 50, fill='black')
+direction = 10
 
-# Create the oval
-circle = drawpad.create_oval(10, 10, 50, 50, fill='green')
-direction = 1
-# Create our animation function
 def animate():
     global direction
-    # Get the x and y co-ordinates of the circle
     x1, y1, x2, y2 = drawpad.coords(circle)
-    if x2 > drawpad.winfo_width(): 
-        direction = - 1
-    elif x1 < 0:
-        direction = 1
-    #Move our oval object by the value of direction
+    if x1 > 800: 
+        drawpad.move(circle,-840,10)
     drawpad.move(circle,direction,0)
-    # Wait for 1 millisecond, then recursively call our animate function
     drawpad.after(1, animate)
 
-# Kick off our animation
+
 animate()
 root.mainloop()
